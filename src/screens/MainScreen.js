b747fd34bf883a655b7c8c43faca1241bbb49b69
Nikita Petrovskiy <n.petrovsky@richbrains.net>
2020-01-22 12:03:26 +0300

    Fixed dependencies, created post component and test data, also updated main screen

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, FlatList } from 'react-native';
+import { DATA } from '../data';
+import { Post } from '../components/Post';
 
 export const MainScreen = ({navigation}) => {
     const goToPost = () => {
         navigation.navigate('Post');
     }
+    
     return (
-        <View style={styles.center}>
-            <Text>Main screen</Text>
-            <Button title="Пернейти на пост" onPress={goToPost} />
+        <View style={styles.wrapper}>
+            <FlatList 
+                data={DATA} 
+                keyExtractor={post => post.id.toString()}
+                renderItem={({ item }) => <Post post={item} />}
+            />
         </View>
     );
 }
@@ -18,9 +24,7 @@ MainScreen.navigationOptions = {
 }
 
 const styles  = StyleSheet.create({
-    center: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+    wrapper: {
+        padding: 10
     }
 });
