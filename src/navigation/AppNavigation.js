bde98dd6c69e6a1585fe0e9e244c4196f8712021
Nikita Petrovskiy <n.petrovsky@richbrains.net>
2020-01-22 14:20:48 +0300

    Created bottom navigator

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,6 +1,11 @@
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { Ionicons } from '@expo/vector-icons';
 import { Platform } from 'react-native';
+
+import { BookedScreen } from '../screens/BookedScreen';
 import { MainScreen } from '../screens/MainScreen';
 import { PostScreen } from '../screens/PostScreen';
 import { THEME } from '../theme';
@@ -8,7 +13,7 @@ import { THEME } from '../theme';
 const PostNavigator = createStackNavigator(
     {
         Main: MainScreen,
-        Post: { screen: PostScreen }
+        Post: PostScreen
     }, 
     { 
         initialRouteName: 'Main',
@@ -21,4 +26,45 @@ const PostNavigator = createStackNavigator(
     }
 );
 
-export const AppNavigation = createAppContainer(PostNavigator);
+const BookedNavigator = createStackNavigator(
+    {
+        Booked: BookedScreen,
+        Post: PostScreen
+    },
+    { 
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff'
+            },
+            headerTintColor: Platform.OS === 'android' ? '#fff' : THEME.MAIN_COLOR
+        } 
+    }
+);
+
+const BottomNavigator = createBottomTabNavigator(
+    {
+        Post: {
+            screen: PostNavigator,
+            navigationOptions: {
+                tabBarIcon : info => (
+                    <Ionicons name="ios-albums" size={25} color={info.tintColor} />
+                )
+            }
+        },
+        Booked: {
+            screen: BookedNavigator,
+            navigationOptions: {
+                tabBarIcon : info => (
+                    <Ionicons name="ios-star" size={25} color={info.tintColor} />
+                )
+            } 
+        }
+    },
+    {
+        tabBarOptions: {
+            activeTintColor: THEME.MAIN_COLOR
+        }
+    }
+);
+
+export const AppNavigation = createAppContainer(BottomNavigator);
