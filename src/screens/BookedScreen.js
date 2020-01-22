3bb7faa1eb3b83e31a32953729eb4e822023da91
Nikita Petrovskiy <n.petrovsky@richbrains.net>
2020-01-22 15:03:55 +0300

    Created booked screen component

--- src/screens/BookedScreen.js
+++ src/screens/BookedScreen.js
@@ -1,18 +1,46 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button, FlatList } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
-export const BookedScreen = ({}) => {
+import { AppHeaderIcon } from '../components/AppHeaderIcon';
+import { DATA } from '../data';
+import { Post } from '../components/Post';
+
+export const BookedScreen = ({navigation}) => {
+    const openPostHandler = post => {
+        navigation.navigate('Post', { 
+            postId: post.id,
+            date: post.date,
+            booked: post.booked
+        });
+    }
+    
     return (
-        <View style={styles.center}>
-            <Text>Booked screen</Text>
+        <View style={styles.wrapper}>
+            <FlatList 
+                data={DATA.filter(post => post.booked)} 
+                keyExtractor={post => post.id.toString()}
+                renderItem={({ item }) => <Post post={item} onOpen={openPostHandler} />}
+            />
         </View>
     );
 }
 
+BookedScreen.navigationOptions = {
+    headerTitle: "Избранные",
+    headerLeft: () => (
+        <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+            <Item 
+                title="Toggle Drawer" 
+                iconName="ios-menu"
+                onPress={() => console.log('Press menu')} 
+            />
+        </HeaderButtons>
+    )
+}
+
 const styles  = StyleSheet.create({
-    center: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+    wrapper: {
+        padding: 10
     }
 });
